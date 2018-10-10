4766b7ddbff6d69efe600e33e519ebfc27ccb804
hyzhou <hyzhou@umich.edu>
2018-10-10 12:58:26 -0400

    changes on laptop.

--- srcGanymede/PIC/Distribution_classify.m
+++ srcGanymede/PIC/Distribution_classify.m
@@ -6,8 +6,8 @@ clear; clc; %close all
 %% Parameters
 cAlfven = 253;
 PlotVType = 2; % 1: v_par vs. v_perp1 2: v_perp1 vs. v_perp2
-Dir = '~/Ganymede/MOP2018/runG8_PIC_1200s/Particles';
-fnameParticle = 'cut_particles0_region0_1_t00000710_n00012900.out';
+Dir = '~/Documents/research/Ganymede/data/DistPlotTest';
+fnameParticle = 'cut_particles1_region0_2_t00000710_n00012900.out';
 fnameField = '3d_fluid_region0_0_t00000710_n00012900.out';
 
 %% Read data
@@ -31,15 +31,15 @@ uz = data.w(:,:,:,uz_);
 
 %% Classify particles based on locations
 Region = cell(4,1);
-% Region{1} = [-2.08 -2.01 -0.05 0.05 0 0.11];
-% Region{2} = [-1.81 -1.72 -0.08 0.08 0 0.11];
-% Region{3} = [-1.95 -1.89 -0.08 0.08 -0.14 -0.04];
-% Region{4} = [-1.86 -1.80 -0.08 0.08 0.36 0.45];
+Region{1} = [-2.04 -2.01 -0.15 0.15 0.03 0.14];
+Region{2} = [-1.81 -1.75 -0.2 0.2 0.03 0.14];
+Region{3} = [-1.94 -1.88 -0.2 0.2 -0.14 -0.04];
+Region{4} = [-1.92 -1.86 -0.2 0.2 0.2 0.3];
 
-Region{1} = [-1.78 -1.75 -0.08 0.08 -0.02 0.11];
-Region{2} = [-1.83 -1.75 -0.08 0.08 -0.02 0.11];
-Region{3} = [-1.85 -1.83 -0.08 0.08 -0.02 0.11];
-Region{4} = [-1.90 -1.85 -0.08 0.08 -0.02 0.11];
+% Region{1} = [-1.87 -1.86 -0.2 0.2 0.02 0.10];
+% Region{2} = [-1.875 -1.87 -0.2 0.2 0.02 0.10];
+% Region{3} = [-1.905 -1.875 -0.2 0.2 0.02 0.10];
+% Region{4} = [-1.92 -1.91 -0.2 0.2 0.02 0.10];
 
 particle = cell(4,1);
 particle{1} = [];
@@ -80,23 +80,25 @@ for ipict=1:4
    dPerp1 = cross([0 -1 0]',dPar); % Perpendicular direction in-plane
    dPerp2 = cross(dPar,dPerp1); % Perpendicular direction out-of-plane
    
-   uPar = particle{ipict}*dPar;
-   uPerp1 = particle{ipict}*dPerp1;
-   uPerp2 = particle{ipict}*dPerp2;
-%    uPar = particle{ipict}(:,3);
-%    uPerp1 = particle{ipict}(:,1);
-%    uPerp2 = particle{ipict}(:,2);
+%    uPar = particle{ipict}*dPar;
+%    uPerp1 = particle{ipict}*dPerp1;
+%    uPerp2 = particle{ipict}*dPerp2;
+   uz = particle{ipict}(:,3);
+   ux = particle{ipict}(:,1);
+   uy = particle{ipict}(:,2);
    
    subplot(2,2,ipict);
    if PlotVType==1
       h = histogram2(uPar/cAlfven,uPerp1/cAlfven);
    elseif PlotVType==2
-      h = histogram2(uPerp1/cAlfven,uPerp2/cAlfven);
+      %h = histogram2(uPerp1/cAlfven,uPerp2/cAlfven);
+      h = histogram2(ux/cAlfven,uz/cAlfven);
+      %h = histogram2(uy/cAlfven,ux/cAlfven);
    else
       error('Unknown PlotVType!')
    end
-   h.XBinLimits = [-3,3];
-   h.YBinLimits = [-3,3];
+   h.XBinLimits = [-2,2];
+   h.YBinLimits = [-2,2];
    h.NumBins = [30 30];
    h.Normalization = 'probability';
    h.FaceColor = 'flat';
@@ -104,16 +106,18 @@ for ipict=1:4
    
    axis equal
    if PlotVType==1
-      xlabel('$u_{\parallel}$','Interpreter','latex')
-      ylabel('$u_{\perp1}$','Interpreter','latex')
+      xlabel('$u_{\parallel}$','Interpreter','latex','FontWeight','bold')
+      ylabel('$u_{\perp1}$','Interpreter','latex','FontWeight','bold')
    elseif PlotVType==2
-      xlabel('$u_{\perp1}$','Interpreter','latex')
-      ylabel('$u_{\perp2}$','Interpreter','latex')
+%       xlabel('$u_{\perp1}$','Interpreter','latex')
+%       ylabel('$u_{\perp2}$','Interpreter','latex')
+      xlabel('$u_x$','Interpreter','latex','FontWeight','bold')
+      ylabel('$u_z$','Interpreter','latex','FontWeight','bold')
    end
    title(sprintf('PIC region %d',ipict))
    colorbar
    view(2)
-   set(gca,'FontSize',16,'LineWidth',1.1)
+   set(gca,'FontSize',18,'LineWidth',1.1)
 end
 
 %% Sample region plot over contour
@@ -136,10 +140,10 @@ func_ = strcmpi(func,filehead.wnames);
 Bx = data.file1.w(:,:,:,func_);
 Bx = permute(Bx,[2 1 3]);
 
-func = 'Ex'; 
+func = 'Ey'; 
 func_ = strcmpi(func,filehead.wnames);
-Ex = data.file1.w(:,:,:,func_);
-Ex = permute(Ex,[2 1 3]);
+Ey = data.file1.w(:,:,:,func_);
+Ey = permute(Ey,[2 1 3]);
 
 func = 'Bz'; 
 func_ = strcmpi(func,filehead.wnames);
@@ -149,21 +153,21 @@ Bz = permute(Bz,[2 1 3]);
 cut1 = squeeze(x(PlaneIndex,:,:));
 cut2 = squeeze(z(PlaneIndex,:,:));
 Bx    = squeeze(Bx(PlaneIndex,:,:));
-Ex    = squeeze(Ex(PlaneIndex,:,:));
+Ey    = squeeze(Ey(PlaneIndex,:,:));
 Bz    = squeeze(Bz(PlaneIndex,:,:));
 [~, ~, Bx] = subsurface(cut1, cut2, Bx, plotrange);
-[~, ~, Ex] = subsurface(cut1, cut2, Ex, plotrange);
+[~, ~, Ey] = subsurface(cut1, cut2, Ey, plotrange);
 [cut1, cut2, Bz] = subsurface(cut1, cut2, Bz, plotrange);
 
 figure(2)
-contourf(cut1,cut2,Ex,50,'Linestyle','none');
+contourf(cut1,cut2,Ey,50,'Linestyle','none');
 colorbar; axis equal; 
 xlabel('x [R_G]'); ylabel('z [R_G]');
-title('Ex');
-set(gca,'FontSize',14,'LineWidth',1.2)
+title('Ey [\mu V/m]');
+set(gca,'FontSize',16,'LineWidth',1.2)
 hold on
 % streamline function requires the meshgrid format strictly
-s = streamslice(cut1',cut2',Bx',Bz',2,'linear');
+s = streamslice(cut1',cut2',Bx',Bz',5,'linear');
 for is=1:numel(s)
    s(is).Color = 'w'; % Change streamline color to white
    s(is).LineWidth = 1.5;
@@ -171,6 +175,7 @@ end
 
 for ipict=1:4
    rectangle('Position',[Region{ipict}(1) Region{ipict}(5) ...
-      Region{ipict}(2)-Region{ipict}(1) Region{ipict}(6)-Region{ipict}(5)])
+      Region{ipict}(2)-Region{ipict}(1) ...
+      Region{ipict}(6)-Region{ipict}(5)],'EdgeColor','r','LineWidth',1.5)
 end
 

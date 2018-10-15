667ea817d59112bf12ee3582046c5c469cd04054
hyzhou <hyzhou@umich.edu>
2018-10-15 16:01:28 -0400

    Commented out unused lines.

--- srcGanymede/PIC/LossCone/get_field.m
+++ srcGanymede/PIC/LossCone/get_field.m
@@ -16,8 +16,8 @@ fnameField = Parameters.fnameField;
 
 data = data.file1;
 
-ne_ = strcmpi('ns0',filehead.wnames);
-ni_ = strcmpi('ni0',filehead.wnames);
+% ne_ = strcmpi('ns0',filehead.wnames);
+% ni_ = strcmpi('ni0',filehead.wnames);
 bx_ = strcmpi('bx',filehead.wnames);
 by_ = strcmpi('by',filehead.wnames);
 bz_ = strcmpi('bz',filehead.wnames);
@@ -27,8 +27,8 @@ bz_ = strcmpi('bz',filehead.wnames);
 xF = data.x(:,:,:,1);       % [Rg]
 yF = data.x(:,:,:,2);       % [Rg]
 zF = data.x(:,:,:,3);       % [Rg]
-ne = data.w(:,:,:,ne_)*1e6;    % [#/m^3]
-ni = data.w(:,:,:,ni_)*1e6/14; % [#/m^3] 
+% ne = data.w(:,:,:,ne_)*1e6;    % [#/m^3]
+% ni = data.w(:,:,:,ni_)*1e6/14; % [#/m^3] 
 Bx = data.w(:,:,:,bx_);      % [nT]
 By = data.w(:,:,:,by_);
 Bz = data.w(:,:,:,bz_);
@@ -36,9 +36,6 @@ Bz = data.w(:,:,:,bz_);
 % Ey = data.w(:,:,:,7)*1e-3; % [mV/m]
 % Ez = data.w(:,:,:,8)*1e-3; % [mV/m]
 
-% The original data is saved in ndgrid format. For streamline and
-% isonormals functions, the input should be in meshgrid format.
-% xF  = permute(x,[2 1 3]);
 
 end
 
